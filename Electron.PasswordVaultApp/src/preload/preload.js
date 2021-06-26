33e3cead970c784f67db4d387245872e271f692f
Mauricio Portillo <mp24@umail.iu.edu>
2021-06-25 20:04:59 -0700

    Initial commit of the project

--- Electron.PasswordVaultApp/src/preload/preload.js
+++ Electron.PasswordVaultApp/src/preload/preload.js
@@ -1,1 +1,25 @@
-﻿import './database-manager.js'
+﻿const { contextBridge, ipcRenderer } = require('electron')
+const mysql = require('mysql2')
+
+function loadWebPage(webPage) {
+    ipcRenderer.send('load-page', webPage);
+};
+
+function popNotification(title, message) {
+    ipcRenderer.send('send-alert', [title, message]);
+};
+
+contextBridge.exposeInMainWorld('connectionAPI', {
+    databaseConnection(config) {
+        let connection = mysql.createConnection(config);
+
+        connection.connect((err) => {
+            if (err) {
+                popNotification("Error", "Invalid credentials. Please try again.");
+            } else {
+                loadWebPage('../src/menu/menu.html');
+            }
+        });
+        connection.end();
+    }
+});
